376e2be2f5dcd3b141532fe84d8776dc606fccc0
Clay Carpenter <claycarpenter@gmail.com>
2015-01-10 20:30:22 -0500

    Removes CordovaLib ant-build directory, files.

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/clay/dev/code/github/pazz-app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/clay/dev/code/github/pazz-app/platforms/android/CordovaLib/AndroidManifest.xml \
