c4291ea26d53d96f9c8f42572cfde9f942f8e688
Clay Carpenter <claycarpenter@gmail.com>
2015-01-10 19:56:56 -0500

    Removes accidentally added Android build products.

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/clay/dev/code/github/pazz-app/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/clay/dev/code/github/pazz-app/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/clay/dev/code/github/pazz-app/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/clay/dev/code/github/pazz-app/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/clay/dev/code/github/pazz-app/platforms/android/ant-build/classes.dex \
-/Users/clay/dev/code/github/pazz-app/platforms/android/CordovaLib/ant-build/classes.jar \
