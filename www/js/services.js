899494a7e2cdc7b410b726056db2df568491944b
Clay Carpenter <claycarpenter@gmail.com>
2015-01-17 17:35:41 -0500

    Adds value providers and services to support random character creation.
    Adds Angular value providers to represent the candidate consonants, vowels, and digits that can be used to build a random password.
    
    Adds Angular service providers to provide random values from the candidate character set pools.

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,34 @@
-angular.module('pazz.app.services', []);
+(function() {
+    var servicesModule = angular.module('pazz.app.services', []);
+    
+    // Define value providers for the three sets of random character 
+    // candidates.
+    servicesModule.value('ConsonantCharSet', 
+        ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']);
+    
+    servicesModule.value('VowelCharSet', 
+        ['a','e','i','o','u']);
+    
+    servicesModule.value('DigitCharSet', 
+        ['0','1','2','3','4','5','6','7','8','9']);
+    
+    var RandomCharacterService = function(charSet) {
+        var getRandomValue = function() {
+            return charSet[Math.floor(Math.random() * charSet.length)];
+        }
+        
+        // Define external interface.
+        this.getRandomValue = getRandomValue;
+    }
+    
+    servicesModule.service('RandomConsonantService', 
+        ['ConsonantCharSet', RandomCharacterService]);
+    
+    servicesModule.service('RandomVowelService', 
+        ['VowelCharSet', RandomCharacterService]);
+    
+    servicesModule.service('RandomDigitService', 
+        ['DigitCharSet', RandomCharacterService]);
+    
+//    servicesModule.Cvccvc99Generator
+})();
